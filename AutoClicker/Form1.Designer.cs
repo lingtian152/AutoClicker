cadeda34aa83413d9c08c1e3281df0d4ec7f048b
lingtian152 <1553396053skwa@gmail.com>
2024-06-08 23:43:47 -0400

    updated files

--- AutoClicker/Form1.Designer.cs
+++ AutoClicker/Form1.Designer.cs
@@ -31,24 +31,26 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.HotKey_Select = new System.Windows.Forms.ComboBox();
+            this.Status = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(50, 39);
+            this.label1.Location = new System.Drawing.Point(82, 82);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 28);
             this.label1.TabIndex = 0;
-            this.label1.Text = "CoolDown";
+            this.label1.Text = "Cooldown";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(50, 127);
+            this.label2.Location = new System.Drawing.Point(96, 157);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(110, 28);
             this.label2.TabIndex = 1;
@@ -57,17 +59,52 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(192, 42);
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox1.Location = new System.Drawing.Point(224, 85);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 25);
+            this.textBox1.Size = new System.Drawing.Size(177, 36);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // HotKey_Select
+            // 
+            this.HotKey_Select.Font = new System.Drawing.Font("宋体", 12F);
+            this.HotKey_Select.FormattingEnabled = true;
+            this.HotKey_Select.Items.AddRange(new object[] {
+            "F1",
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6",
+            "F7",
+            "F8",
+            "F9",
+            "F10"});
+            this.HotKey_Select.Location = new System.Drawing.Point(224, 161);
+            this.HotKey_Select.Name = "HotKey_Select";
+            this.HotKey_Select.Size = new System.Drawing.Size(177, 28);
+            this.HotKey_Select.TabIndex = 4;
+            this.HotKey_Select.SelectedIndexChanged += new System.EventHandler(this.HotKey_Select_SelectedIndexChanged);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Status.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Status.Location = new System.Drawing.Point(119, 226);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(253, 30);
+            this.Status.TabIndex = 6;
+            this.Status.Text = "Status: {Status}";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 352);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.HotKey_Select);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -83,6 +120,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox HotKey_Select;
+        private System.Windows.Forms.Label Status;
     }
 }
 
--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -1,25 +1,213 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
+using SharpConfig;
 
 namespace AutoClicker
 {
     public partial class Form1 : Form
     {
+        static string FileName = "settings.ini";
+        int clickInterval = 100; // Default Click CoolDown
+        string HotKey = "F6"; // Default HotKey
+        bool isClicking = false;
+        Thread clickingThread;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+
+        const int HOTKEY_ID = 1; // ID for the hotkey
+
         public Form1()
         {
             InitializeComponent();
+            this.Status.Text = "Status: Off";
+
+            // Load settings
+            LoadSettings();
+
+            this.textBox1.Text = clickInterval.ToString();
+            this.HotKey_Select.Text = HotKey;
+
+            this.textBox1.TextChanged += new EventHandler(textBox1_TextChanged);
+            this.HotKey_Select.SelectedIndexChanged += new EventHandler(HotKey_Select_SelectedIndexChanged);
+
+            RegisterHotKey();
+        }
+
+        private void LoadSettings()
+        {
+            if (!File.Exists(FileName))
+            {
+                using (File.Create(FileName)) { }
+            }
+
+            try
+            {
+                var configFile = Configuration.LoadFromFile(FileName);
+                var section = configFile["Settings"];
+
+                if (section.Contains("ClickInterval"))
+                {
+                    clickInterval = section["ClickInterval"].IntValue;
+                }
+                else
+                {
+                    section["ClickInterval"].IntValue = clickInterval;
+                }
+
+                if (section.Contains("HotKey"))
+                {
+                    HotKey = section["HotKey"].StringValue;
+                }
+                else
+                {
+                    section["HotKey"].StringValue = HotKey;
+                }
+
+                configFile.SaveToFile(FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load settings: {ex.Message}");
+            }
+        }
+
+        public void SaveSettings(string key, object value)
+        {
+            try
+            {
+                var configFile = Configuration.LoadFromFile(FileName);
+                var section = configFile["Settings"];
+
+                if (value is string stringValue)
+                {
+                    section[key].StringValue = stringValue;
+                }
+                else if (value is int intValue)
+                {
+                    section[key].IntValue = intValue;
+                }
+                else
+                {
+                    throw new ArgumentException("Unsupported value type.");
+                }
+
+                configFile.SaveToFile(FileName);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Failed to save settings: {e.Message}");
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            if (!int.TryParse(this.textBox1.Text, out int clickInterval))
+            {
+                MessageBox.Show("Please enter a number");
+                return;
+            }
 
+            this.clickInterval = clickInterval;
+            SaveSettings("ClickInterval", clickInterval);
+        }
+
+        private void HotKey_Select_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HotKey = this.HotKey_Select.Text;
+            SaveSettings("HotKey", HotKey);
+            RegisterHotKey(); // Re-register the hotkey
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartClicking();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StopClicking();
+        }
+
+        private void StartClicking()
+        {
+            if (!isClicking)
+            {
+                isClicking = true;
+                this.Status.Text = "Status: On";
+                clickingThread = new Thread(ClickingLoop);
+                clickingThread.Start();
+            }
+        }
+
+        private void StopClicking()
+        {
+            if (isClicking)
+            {
+                isClicking = false;
+                clickingThread?.Join();
+                this.Status.Text = "Status: Off";
+            }
+        }
+
+        private void ClickingLoop()
+        {
+            while (isClicking)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                Thread.Sleep(clickInterval);
+            }
+        }
+
+        private void RegisterHotKey()
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID); // Unregister any existing hotkey
+
+            uint vk = (uint)Enum.Parse(typeof(Keys), HotKey, true);
+            bool registered = RegisterHotKey(this.Handle, HOTKEY_ID, 0, vk);
+
+            if (!registered)
+            {
+                MessageBox.Show("Failed to register hotkey.");
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                if (isClicking)
+                {
+                    StopClicking();
+                }
+                else
+                {
+                    StartClicking();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+            base.OnFormClosing(e);
         }
     }
 }
