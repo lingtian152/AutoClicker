b1270aa1065e7778e334b791de6f24db82fe4a40
lingtian152 <1553396053skwa@gmail.com>
2024-06-10 21:36:04 -0400

    Separated save Setting function

--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -1,31 +1,24 @@
 ﻿using System;
+using System.Drawing.Printing;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Forms;
 using SharpConfig;
+
 
 
 namespace AutoClicker
 {
     public partial class Form1 : Form
     {
-        
-        KeyboardHook keyboardHook; // 键盘钩子
-
         static string FileName = "settings.ini";
         int clickInterval = 100; // Default Click CoolDown
-        string HotKey = "F1"; // Default HotKey
+        string HotKey = "F6"; // Default HotKey
         bool isClicking = false;
 
+        AutoClicker autoClicker = new AutoClicker();
+        KeyboardHook keyboardHook;
 
-        Thread clickThread;
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern int mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
-
-        const int MOUSEEVENTF_LEFTDOWN = 0x0002; // 鼠标左键按下
-        const int MOUSEEVENTF_LEFTUP = 0x0004; // 鼠标左键抬起
+        static Action<string, object> SaveSettings = (key, value) => ConfigurationManager.SaveSettings(FileName, key, value);
 
         public Form1()
         {
@@ -37,49 +30,17 @@ namespace AutoClicker
 
             this.textBox1.Text = clickInterval.ToString();
             this.HotKey_Select.Text = HotKey;
-            this.textBox1.TextChanged += new EventHandler(Cooldown_Changed);
 
-            // 初始化 键盘钩子
+            this.textBox1.TextChanged += new EventHandler(Cooldown_Changed);
+            this.HotKey_Select.SelectedIndexChanged += new EventHandler(HotkeySelec_Changed);
+
+            // Initialize and start the keyboard hook
             keyboardHook = new KeyboardHook();
             keyboardHook.KeyDownEvent += new KeyEventHandler(Hook_KeyDown);
             keyboardHook.Start();
         }
 
-        private void Cooldown_Changed(object sender, EventArgs e)
-        {
-            if (!int.TryParse(this.textBox1.Text, out int clickInterval))
-            {
-                MessageBox.Show("Please enter a number");
-                return;
-            } else if (clickInterval <= 0)
-            {
-                MessageBox.Show("Please enter a number greater than 0");
-                this.textBox1.Text = 100.ToString();
-                return;
-            }
-
-            clickInterval = int.Parse(this.textBox1.Text);
-            SaveSettings("ClickInterval", clickInterval);
-        }
-
-        private void closeButton_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void MinimiButton_Click(object sender, EventArgs e) // 最小化按钮
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void HotkeySelec_Changed(object sender, EventArgs e) // 热键选择
-        {
-            this.HotKey = this.HotKey_Select.Text;
-
-            SaveSettings("HotKey", HotKey);
-        }
-
-        private void LoadSettings() // 加载设置
+        private void LoadSettings()
         {
             if (!File.Exists(FileName))
             {
@@ -117,31 +78,42 @@ namespace AutoClicker
             }
         }
 
-        public void SaveSettings(string key, object value) // 保存设置
+        private void Cooldown_Changed(object sender, EventArgs e)
         {
-            try
+            if (!int.TryParse(this.textBox1.Text, out int interval))
             {
-                var configFile = Configuration.LoadFromFile(FileName);
-                var section = configFile["Settings"];
+                MessageBox.Show("Please enter a valid number");
+                return;
+            } else if (clickInterval <= 0 || this.textBox1.Text == "0")
+            {
+                MessageBox.Show("Please enter a number greater than 0");
+                this.textBox1.Text = "100";
+                clickInterval = 100;
+                return;
+            }
 
-                if (value is string stringValue)
+            this.clickInterval = interval;
+            SaveSettings("ClickInterval", interval);
+        }
+
+        private void HotkeySelec_Changed(object sender, EventArgs e)
+        {
+            HotKey = this.HotKey_Select.Text;
+            SaveSettings("HotKey", HotKey);
+        }
+
+        private void Hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == HotKey)
+            {
+                if (isClicking)
                 {
-                    section[key].StringValue = stringValue;
-                }
-                else if (value is int intValue)
-                {
-                    section[key].IntValue = intValue;
+                    StopClicking();
                 }
                 else
                 {
-                    throw new ArgumentException("Unsupported value type.");
+                    StartClicking();
                 }
-
-                configFile.SaveToFile(FileName);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show($"Failed to save settings: {e.Message}");
             }
         }
 
@@ -151,6 +123,7 @@ namespace AutoClicker
             {
                 isClicking = true;
                 this.Status.Text = "Status: On";
+                autoClicker.StartClick(clickInterval);
             }
         }
 
@@ -160,48 +133,14 @@ namespace AutoClicker
             {
                 isClicking = false;
                 this.Status.Text = "Status: Off";
+                autoClicker.StopClick();
             }
         }
 
-        private void ClickingLoop()
+        protected override void OnFormClosing(FormClosingEventArgs e)
         {
-            while (isClicking)
-            {
-                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-                Thread.Sleep(clickInterval);
-            }
-        }
-
-        private void Hook_KeyDown(object sender, KeyEventArgs e) // 按下热键
-        {
-            if (e.KeyCode.ToString() == HotKey)
-            {
-              if (isClicking)
-              {
-                  StopClicking();
-              }
-              else
-              {
-                  StartClicking();
-                  clickThreadStart();         
-              }                
-            }
-        }
-
-        private void clickThreadStart()
-        {
-            clickThread = new Thread(ClickingLoop);
-            clickThread.Start();
-        }
-
-        protected override void OnFormClosing(FormClosingEventArgs e) // 关闭窗口
-        {
-            keyboardHook.Stop();
-            if (clickThread != null)
-            {
-                clickThread.Abort();
-            }
+            keyboardHook?.Stop();
+            autoClicker.StopClick();
             base.OnFormClosing(e);
         }
     }
---
+++ AutoClicker/SaveSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using SharpConfig;
+
+namespace AutoClicker
+{
+   class ConfigurationManager
+    {
+        public static void SaveSettings(string fileName, string key, object value)
+        {
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName).Close();
+            }
+
+            var configFile = Configuration.LoadFromFile(fileName);
+            var section = configFile["Settings"];
+
+            if (value is string stringValue)
+            {
+                section[key].StringValue = stringValue;
+            }
+            else if (value is int intValue)
+            {
+                section[key].IntValue = intValue;
+            }
+            else if (value is bool boolValue)
+            {
+                section[key].BoolValue = boolValue;
+            }
+
+            configFile.SaveToFile(fileName);
+        }
+    }
+
+}
