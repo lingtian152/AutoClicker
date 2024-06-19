862067e1599b02a6755ff44dffaa0c86cde0a506
lingtian152 <1553396053skwa@gmail.com>
2024-06-18 21:10:34 -0400

    updated files

--- AutoClicker/Properties/Resources.Designer.cs
+++ AutoClicker/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace AutoClicker.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap setting {
+            get {
+                object obj = ResourceManager.GetObject("setting", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap success {
             get {
                 object obj = ResourceManager.GetObject("success", resourceCulture);
