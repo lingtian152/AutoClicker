f1eb8876d4aea6ba2962785c532ba7c6ad4d338f
lingtian152 <1553396053skwa@gmail.com>
2024-06-18 21:13:28 -0400

    feat: added setting

---
+++ AutoClicker/src/setting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AutoClicker
+{
+    public partial class setting : Form
+    {
+        public setting()
+        {
+            InitializeComponent();
+        }
+
+        private void close_button_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void check_for_update_click(object sender, EventArgs e)
+        {
+            Updater updater = new Updater();
+            updater.Show();
+        }
+    }
+}
