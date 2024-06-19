639205fd6a317132f5995890ba14bffbb352b0a7
lingtian152 <1553396053skwa@gmail.com>
2024-06-19 14:52:32 -0400

    feat: added installer

---
+++ Installer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Installer
+{
+    partial class Installer
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Installer_button = new System.Windows.Forms.Button();
+            this.close_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 88);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(357, 41);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Installer_button
+            // 
+            this.Installer_button.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Installer_button.Location = new System.Drawing.Point(114, 159);
+            this.Installer_button.Name = "Installer_button";
+            this.Installer_button.Size = new System.Drawing.Size(145, 55);
+            this.Installer_button.TabIndex = 1;
+            this.Installer_button.Text = "Install";
+            this.Installer_button.UseVisualStyleBackColor = true;
+            this.Installer_button.Click += new System.EventHandler(this.Installer_button_Click);
+            // 
+            // close_button
+            // 
+            this.close_button.FlatAppearance.BorderSize = 0;
+            this.close_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_button.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.close_button.Location = new System.Drawing.Point(337, 13);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(32, 36);
+            this.close_button.TabIndex = 2;
+            this.close_button.Text = "X";
+            this.close_button.UseVisualStyleBackColor = true;
+            this.close_button.Click += new System.EventHandler(this.close_button_click);
+            // 
+            // Installer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 270);
+            this.Controls.Add(this.close_button);
+            this.Controls.Add(this.Installer_button);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Installer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Installer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Installer_button;
+        private System.Windows.Forms.Button close_button;
+    }
+}
+
---
+++ Installer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using Ionic.Zip;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Installer
+{
+    public partial class Installer : Form
+    {
+        public string currentPath = "./";
+        private string downloadUrl = "https://github.com/lingtian152/AutoClicker/releases/latest/download/AutoClicker.zip"; // Replace with your actual download URL
+        private string tempDownloadPath = Path.Combine(Path.GetTempPath(), "AutoClicker.zip");
+        private bool isDownloading;
+
+        public Installer()
+        {
+            InitializeComponent();
+            //CheckAndDeletePendingOverwriteFiles();
+        }
+
+        private void close_button_click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Installer_button_Click(object sender, EventArgs e)
+        {
+            if (!isDownloading)
+            {
+                isDownloading = true;
+                DownloadFile();
+            }
+        }
+
+        private void DownloadFile()
+        {
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressCallback);
+                    wc.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadFileCompletedCallback);
+                    wc.DownloadFileAsync(new Uri(downloadUrl), tempDownloadPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Download and upgrade error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                isDownloading = false;
+            }
+        }
+
+        private void DownloadProgressCallback(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update your progress bar here
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void DownloadFileCompletedCallback(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("Download error: " + e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                isDownloading = false;
+                return;
+            }
+
+            try
+            {
+                ExtractFiles(tempDownloadPath, currentPath);
+                CleanUp();
+                MessageBox.Show("Installation completed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Extraction error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                isDownloading = false;
+            }
+        }
+
+        private void ExtractFiles(string zipFilePath, string extractPath)
+        {
+            using (ZipFile zip = ZipFile.Read(zipFilePath))
+            {
+                zip.ExtractAll(extractPath, ExtractExistingFileAction.OverwriteSilently);
+            }
+        }
+
+        private void CleanUp()
+        {
+            try
+            {
+                string filename = Path.Combine(".\\AutoClicker.exe");
+                if (File.Exists(filename))
+                {
+                    Process.Start(filename);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cleanup error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } finally
+            {
+
+               Application.Exit();
+            }
+        }
+    }
+}
