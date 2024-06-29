6896bb2ce2a4b4df4d2487a0d567f87c3a76cc33
lingtian152 <1553396053skwa@gmail.com>
2024-06-29 12:53:44 -0400

    refactor: Added DragForm.cs to Utilities This is a separate class from any form that is need to be dragged. it already separate from form code.

---
+++ AutoClicker/src/Utilities/DragForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AutoClicker.src.Utilities
+{
+    class DragForm
+    {
+        // 拖动窗体
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public static void StartDrag(IntPtr handle)
+        {
+            ReleaseCapture();
+            SendMessage(handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+    }
+}
